901fb3d09e650fb793d8c872af7ffd829eae6bfe
mkuznetsova <maria.kuznetsova.05.11@gmail.com>
2016-03-03 23:57:50 +0400

    blah

--- build.sbt
+++ build.sbt
@@ -12,16 +12,15 @@ libraryDependencies ++= Seq(
   "org.mongodb.scala" %% "mongo-scala-driver" % "1.1.0",
   "com.stratio.datasource" %% "spark-mongodb" % "0.10.0",
   "org.apache.spark" %% "spark-mllib" % "1.5.2",
-  "org.apache.hadoop" % "hadoop-yarn-api" % "2.2.0",
-  "org.apache.hadoop" % "hadoop-mapreduce-client-core" % "2.2.0",
-  "org.apache.hadoop" % "hadoop-mapreduce-client-jobclient" % "2.2.0",
-  "org.apache.hadoop" % "hadoop-mapreduce-client-jobclient" % "2.6.0",
-  "org.apache.hadoop" % "hadoop-mapreduce-client-shuffle" % "2.6.0",
-  "org.apache.hadoop" % "hadoop-mapreduce-client-common" % "2.6.0",
-  "org.apache.hadoop" % "hadoop-mapreduce-client-app" % "2.6.0",
-  "org.mongodb.mongo-hadoop" % "mongo-hadoop-core" % "1.4.2"
+  "org.mongodb.mongo-hadoop" % "mongo-hadoop-core" % "1.4.2",
+  "org.scala-lang" % "scala-compiler" % "2.11.7",
+  "org.apache.commons" % "commons-lang3" % "3.3.2",
+  "jline" % "jline" % "2.12.1",
+  "org.scala-lang.modules" %% "scala-xml" % "1.0.4",
+  "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4",
+  "org.slf4j" % "slf4j-api" % "1.7.10"
 )
 
-resolvers += "Spark Packages Repo" at "http://dl.bintray.com/spark-packages/maven"
+//resolvers += "Spark Packages Repo" at "http://dl.bintray.com/spark-packages/maven"
 
 // resolvers += Resolver.mavenLocal
--- src/main/scala/artezio/mkuznetsova/MainObject.scala
+++ src/main/scala/artezio/mkuznetsova/MainObject.scala
@@ -3,57 +3,56 @@ package artezio.mkuznetsova
 import java.util.Date
 
 import com.mongodb.Mongo
-import org.apache.spark.{SparkConf, SparkContext}
-
-import org.apache.spark.mllib.clustering.KMeans
-import org.apache.spark.mllib.linalg.{DenseVector, Vectors}
-import org.bson.BSONObject
-import org.apache.hadoop.conf.Configuration
 import com.mongodb.casbah.Imports._
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.{SparkConf, SparkContext}
+import org.bson.BSONObject
 
 /**
  * Created by mkuznetsova on 12/8/2015.
  */
-object MainObject extends App{
+object MainObject extends App {
   println("!!! Start !!!")
 
   val sparkConf = new SparkConf().setAppName("stress_zones").setMaster("local[2]")
   val sc = new SparkContext(sparkConf)
 
   val numClusters = 5
-  val numIterations=20
+  val numIterations = 20
 
-/* Kmeans from texp file */
-//  val pulse3 = sc.textFile("D:/bigdata_pulse3.cvs")
-//  val parsedDate3 = pulse3.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
-//  val clusters3 = KMeans.train(parsedDate3, numClusters, numIterations)
+  /* Kmeans from texp file */
+  //  val pulse3 = sc.textFile("D:/bigdata_pulse3.cvs")
+  //  val parsedDate3 = pulse3.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
+  //  val clusters3 = KMeans.train(parsedDate3, numClusters, numIterations)
 
 
-  /********  MONGO ****/
+  /** ******  MONGO ****/
   val mongoConfig = new Configuration()
   //mongoConfig.set("mongo.input.uri", "mongodb://192.168.1.32:27017/cardio.user_date")
   mongoConfig.set("mongo.input.uri", "mongodb://localhost:27017/cardio.user_date")
   mongoConfig.set("mongo.job.input.format", "com.mongodb.hadoop.MongoInputFormat")
 
   val mongoRDD = sc.newAPIHadoopRDD(mongoConfig, classOf[com.mongodb.hadoop.MongoInputFormat], classOf[Object], classOf[BSONObject])
-  val bsons= mongoRDD.values
-  val filtredBsons = bsons.filter(b => (Double.unbox(b.get("pulse")) > 70))
+  val bsons = mongoRDD.values
+  val filtredBsons = bsons.filter(b => Double.unbox(b.get("pulse")) > 70)
 
-  val vectors = filtredBsons.map( s => Vectors.dense(Array(Double.unbox(s.get("location_lan")), Double.unbox(s.get("location_lon"))))).cache()
+  val vectors = filtredBsons.map(s => Vectors.dense(Array(Double.unbox(s.get("location_lan")), Double.unbox(s.get("location_lon"))))).cache()
 
-  println("--------------------------- KMeans start ---------------------");
+  println("--------------------------- KMeans start ---------------------")
   val clusters = KMeans.train(vectors, numClusters, numIterations)
-  println("--------------------------- KMeans end  -----------------------");
+  println("--------------------------- KMeans end  -----------------------")
 
   val vector = clusters.clusterCenters
 
   //var mongo = new Mongo("192.168.1.32")
-  var mongo = new Mongo("localhost")
-  var db = mongo.getDB("cardio")
-  var coll = db.getCollection("pulse")
+  val mongo = new Mongo("localhost")
+  val db = mongo.getDB("cardio")
+  val coll = db.getCollection("pulse")
 
   vector.map(s => {
-    var obj = new BasicDBObject()
+    val obj = new BasicDBObject()
     obj.put("date", new Date().getTime().toString)
     obj.put("lan", s.toArray.apply(0).toString)
     obj.put("lon", s.toArray.apply(1).toString)
