00f9dc9dfa75710dbce4f26703b47ea88be1d677
mkuznetsova <maria.kuznetsova.05.11@gmail.com>
2016-03-19 10:30:31 +0400

    blah

--- src/main/scala/artezio/mkuznetsova/MainObject.scala
+++ src/main/scala/artezio/mkuznetsova/MainObject.scala
@@ -1,6 +1,7 @@
 package artezio.mkuznetsova
 
-import java.util.Date
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
 
 import com.mongodb.Mongo
 import com.mongodb.casbah.Imports._
@@ -15,30 +16,43 @@ import org.bson.BSONObject
  */
 object MainObject extends App {
   println("!!! Start !!!")
+  val format = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy")
 
-  val sparkConf = new SparkConf().setAppName("stress_zones").setMaster("local[2]")
+  val sparkConf = new SparkConf().setAppName("stress_zones")//.setMaster("local[2]")
   val sc = new SparkContext(sparkConf)
 
   val numClusters = 5
   val numIterations = 20
 
-  /* Kmeans from texp file */
-  //  val pulse3 = sc.textFile("D:/bigdata_pulse3.cvs")
-  //  val parsedDate3 = pulse3.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
-  //  val clusters3 = KMeans.train(parsedDate3, numClusters, numIterations)
-
-
   /** ******  MONGO ****/
   val mongoConfig = new Configuration()
-  //mongoConfig.set("mongo.input.uri", "mongodb://192.168.1.32:27017/cardio.user_date")
-  mongoConfig.set("mongo.input.uri", "mongodb://localhost:27017/cardio.user_date")
+//  mongoConfig.set("mongo.input.uri", "mongodb://192.168.1.32:27017/cardio.data")
+  mongoConfig.set("mongo.input.uri", "mongodb://192.168.1.32:27017/cardio_test_db.pulse_data_test")
   mongoConfig.set("mongo.job.input.format", "com.mongodb.hadoop.MongoInputFormat")
 
   val mongoRDD = sc.newAPIHadoopRDD(mongoConfig, classOf[com.mongodb.hadoop.MongoInputFormat], classOf[Object], classOf[BSONObject])
-  val bsons = mongoRDD.values
-  val filtredBsons = bsons.filter(b => Double.unbox(b.get("pulse")) > 70)
 
-  val vectors = filtredBsons.map(s => Vectors.dense(Array(Double.unbox(s.get("location_lan")), Double.unbox(s.get("location_lon"))))).cache()
+  println("**********************************************")
+  println(mongoRDD)
+
+  val bsons = mongoRDD.values
+  println("**********************************************")
+  println(bsons)
+  println(bsons.count())
+
+  val calendar = Calendar.getInstance();
+  calendar.add(Calendar.HOUR, -24);
+  val earlyDate = calendar.getTime();
+
+ // val filtredBsons = bsons.filter(b => Integer.decode(b.get("pulse").toString) > 70 && (format.parse(b.get("dt").toString)).after(earlyDate))
+ // val filtredBsons = bsons.filter(b => Integer.decode(b.get("pulse").toString) > 70 && (format.parse(b.get("dt").toString)).before(new Date()))
+   val filtredBsons = bsons.filter(b => Integer.decode(b.get("heartRateMeasurement").toString) > 70
+                                        && b.get("longitude")!=null
+                                        && b.get("latitude")!=null)
+
+
+  val vectors = filtredBsons.map(s => Vectors.dense(Array(Double.unbox(s.get("longitude")), Double.unbox(s.get("latitude"))))).cache()
+//  val vectors = filtredBsons.map(s => Vectors.dense(Array(Double.unbox(s.get("location_lan")), Double.unbox(s.get("location_lon"))))).cache()
 
   println("--------------------------- KMeans start ---------------------")
   val clusters = KMeans.train(vectors, numClusters, numIterations)
@@ -46,19 +60,30 @@ object MainObject extends App {
 
   val vector = clusters.clusterCenters
 
-  //var mongo = new Mongo("192.168.1.32")
-  val mongo = new Mongo("localhost")
-  val db = mongo.getDB("cardio")
-  val coll = db.getCollection("pulse")
 
+  var mongo = new Mongo("192.168.1.32")
+ // val mongo = new Mongo("localhost")
+  //val db = mongo.getDB("cardio")
+  val db = mongo.getDB("cardio_test_db")
+  val coll = db.getCollection("stress")
+
+  val obj = new BasicDBObject()
+  obj.put("date", new Date())
+  val sb = new StringBuilder()
+  sb.append("[")
   vector.map(s => {
-    val obj = new BasicDBObject()
-    obj.put("date", new Date().getTime().toString)
-    obj.put("lan", s.toArray.apply(0).toString)
-    obj.put("lon", s.toArray.apply(1).toString)
-    coll.insert(obj)
-    coll.save(obj)
+    sb.append("[")
+    .append( s.toArray.apply(0).toString)
+    .append( ",")
+    .append(s.toArray.apply(1).toString)
+    .append("]")
   })
+  sb.append("]")
+
+  obj.put("locations", sb.toString())
+
+  coll.insert(obj)
+  coll.save(obj)
 
   println("!!! Finish !!!")
 }
